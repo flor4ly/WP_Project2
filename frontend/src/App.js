675950d7a7b1ed2d932a8c88e844b2c64d8fd88a
Xurliman Gobenova <X.gobenova@newuu.uz>
2025-04-17 20:21:19 +0500

    careers page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import HomePage from './presentation/home/home_page';
-
+import Careers from './presentation/careers/careers_page';
 function App() {
   return (
     <div className="App">
-  
-      <HomePage />
+      <Careers/>
     </div>
   );
 }
