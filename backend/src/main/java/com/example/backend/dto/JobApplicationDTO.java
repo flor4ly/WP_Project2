730e5b7b77097ff33032337f8b619ba0279519ea
Ruxshona Baymanova <rbaymanova@gmail.com>
2025-04-20 00:49:53 +0500

    JobApplicationController edited

--- backend/src/main/java/com/example/backend/dto/JobApplicationDTO.java
+++ backend/src/main/java/com/example/backend/dto/JobApplicationDTO.java
@@ -6,19 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class JobApplicationDTO {
-    private Long id; // Primary key
     private String applicantName;
     private String applicantPhone;
-    private String resumePath;
     private Long jobId;
-    public JobApplicationDTO() {}
-
-    public JobApplicationDTO(Long id, String applicantName, String applicantPhone,
-                             String resumePath, Long jobId) {
-        this.id = id;
-        this.applicantName = applicantName;
-        this.applicantPhone = applicantPhone;
-        this.resumePath = resumePath;
-        this.jobId = jobId;
-    }
 }
