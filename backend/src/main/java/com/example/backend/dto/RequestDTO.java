95be8c1c52c5b1625583dc54d991d989f437d18a
Ruxshona Baymanova <rbaymanova@gmail.com>
2025-04-20 00:51:42 +0500

    Small edit of RequestDTO

--- backend/src/main/java/com/example/backend/dto/RequestDTO.java
+++ backend/src/main/java/com/example/backend/dto/RequestDTO.java
@@ -1,8 +1,5 @@
 package com.example.backend.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
