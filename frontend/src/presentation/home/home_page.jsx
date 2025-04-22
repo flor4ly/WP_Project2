94f13b17841f293253ce2f4e63c201d056287207
Ruxshona Baymanova <rbaymanova@gmail.com>
2025-04-22 15:02:55 +0500

    Home Page connected to DB

--- frontend/src/presentation/home/home_page.jsx
+++ frontend/src/presentation/home/home_page.jsx
@@ -118,7 +118,7 @@ export default function HomePage() {
       </section>
 
       <CollaboratorsSection />
-      <TestimonialsSection />
+      <TestimonialsSection jobListings={homeData.jobListings} />
     </div>
   );
 }
