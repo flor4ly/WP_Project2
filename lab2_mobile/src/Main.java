f49211ecd216079ad5253b99c65c725f9ef74ac2
lobarashurova <lobaroyxon6169@gmail.com>
2025-09-03 09:30:09 +0500

    initial commit000

--- lab2_mobile/src/Main.java
+++ lab2_mobile/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        
+      task1(10);
+      /// task2 answer:
+    }
+
+
+    static void task1(int numbers) {
+        for (int i = 0; i < numbers; i++) {
+            System.out.println("numbers index :: " + i);
+        }
     }
 }
+//000
+
